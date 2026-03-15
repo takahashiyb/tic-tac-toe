609745f8b0d6e9cf1a388a00cecf7460b4db6847
takahashiyb <takahashiyb89@gmail.com>
2026-03-16 03:51:16 +0800

    Set up route for MultPage, the mulitplayer page

---
+++ src/pages/MultiPage.vue
@@ -0,0 +1,7 @@
+<script lang="ts" setup>
+import GameButtons from '@/component/GameButtons.vue'
+</script>
+<template>
+  <GameButtons></GameButtons>
+</template>
+<style lang="scss" scoped></style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,9 @@
+import MultiPage from '@/pages/MultiPage.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [{ path: '/', name: 'multiplayer', component: MultiPage }],
 })
 
 export default router
