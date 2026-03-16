4c70820589bad802874a3ad6744d6a6058e535ac
takahashiyb <takahashiyb89@gmail.com>
2026-03-17 03:07:45 +0800

    Initial commit for CpuPage

---
+++ src/pages/CpuPage.vue
@@ -0,0 +1,120 @@
+<script lang="ts" setup>
+import GameButtons from '@/component/GameButtons.vue'
+import ScoreTable from '@/component/ScoreTable.vue'
+
+import { useGameStore } from '@/stores/game'
+import MenuDialog from '@/component/MenuDialog.vue'
+import { onBeforeMount } from 'vue'
+
+const game = useGameStore()
+
+onBeforeMount(() => {
+  if (localStorage) {
+    game.restoreState()
+  }
+})
+</script>
+<template>
+  <dialog :open="game.gameState === 'end'">
+    <div class="dialog__container">
+      <span class="dialog__pretext">{{
+        game.winner && game.winner.player.toUpperCase() + ' WINS!'
+      }}</span>
+      <div class="dialog__header">
+        <img src="@/assets/icons/icon-x.svg" alt="x icon" v-if="game.winner && game.turn === 0" />
+        <img src="@/assets/icons/icon-o.svg" alt="o icon" v-if="game.winner && game.turn === 1" />
+        <span
+          :class="{ teal: game.winner && game.turn === 0, amber: game.winner && game.turn === 1 }"
+          >{{ game.winner ? 'TAKES THE ROUND' : 'ROUND TIED' }}</span
+        >
+      </div>
+      <RouterLink class="dialog__quit dialog__buttons" :to="{ name: 'start' }">QUIT</RouterLink>
+      <span class="dialog__next dialog__buttons" @click="game.newGame">NEXT ROUND</span>
+    </div>
+  </dialog>
+  <MenuDialog></MenuDialog>
+  <header>
+    <img src="@/assets/icons/logo.svg" alt="icon of logo" />
+    <div class="turn-container">
+      <img class="x-mark" src="@/assets/icons/icon-x.svg" alt="x icon" v-if="game.turn === 0" />
+      <img class="o-mark" src="@/assets/icons/icon-o.svg" alt="o icon" v-if="game.turn === 1" />
+      <span class="turn-indicator">TURN</span>
+    </div>
+    <div class="button__restart" @click="game.menuRestart = true">
+      <span class="sr-only">redirects to game set up page</span>
+      <img src="@/assets/icons/icon-restart.svg" alt="" />
+    </div>
+  </header>
+  <main>
+    <GameButtons></GameButtons>
+    <ScoreTable></ScoreTable>
+  </main>
+</template>
+<style lang="scss" scoped>
+@use '/src/assets/styles/main.scss' as v;
+
+header {
+  width: 100%;
+  max-width: 424px;
+
+  padding: v.$spacing-300;
+
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  justify-content: stretch;
+  align-items: stretch;
+}
+
+main {
+  height: 100%;
+
+  display: grid;
+  align-content: center;
+}
+
+.turn-container {
+  height: 40px;
+  width: 96px;
+  background-color: v.$slate-800;
+
+  padding: v.$spacing-150;
+
+  border-radius: v.$radius-06;
+
+  box-shadow: 0 4px rgba(v.$neutral-950, 20%);
+
+  display: flex;
+  align-items: center;
+  gap: v.$spacing-100;
+
+  justify-self: center;
+}
+
+.x-mark,
+.o-mark {
+  height: 100%;
+}
+
+.x-mark {
+  filter: invert(1) saturate(0.1) hue-rotate(180deg) brightness(2.3);
+}
+
+.o-mark {
+  filter: invert(1) saturate(0.05) hue-rotate(300deg) brightness(2.7);
+}
+
+.button__restart {
+  background-color: v.$slate-300;
+  height: 40px;
+
+  padding: v.$spacing-150;
+
+  border-radius: v.$radius-06;
+
+  box-shadow: 0 4px rgba(v.$slate-300, 40%);
+
+  justify-self: flex-end;
+
+  cursor: pointer;
+}
+</style>
