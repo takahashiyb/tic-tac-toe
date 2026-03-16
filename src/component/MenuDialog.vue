44a7664293f25eabd6b5d1372115e8d31e744448
takahashiyb <takahashiyb89@gmail.com>
2026-03-17 03:07:14 +0800

    Separate MenuDialog component

---
+++ src/component/MenuDialog.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+import { useGameStore } from '@/stores/game'
+
+const game = useGameStore()
+</script>
+<template>
+  <dialog :open="game.menuRestart === true">
+    <div class="dialog__container">
+      <span class="dialog__header">RESTART GAME?</span>
+      <span class="dialog__resume dialog__buttons" @click="game.menuRestart = false"
+        >NO, CANCEL</span
+      >
+      <RouterLink :to="{ name: 'start' }" class="dialog__setup dialog__buttons"
+        >YES, RESTART</RouterLink
+      >
+    </div>
+  </dialog>
+</template>
